a4f30bd9e660c4d0f5b866cf115b391473a36a38
Jai Gawande <75380070+Jai-eng-gif@users.noreply.github.com>
2023-07-11 17:19:07 +0530

    Update desktop_notify.py

--- desktop_notify.py
+++ desktop_notify.py
@@ -5,11 +5,10 @@ if __name__ == '__main__':
     while True:
         notification.notify(
             title="Drink Water",
-            message="drink water half hour over",
+            message="It's time to drink some water and quench your thirst. Remember, proper hydration is essential for your well-being and overall health.",
             app_icon="D:/Python Project/water.ico",
             timeout=5
         )
         time.sleep(3600)
 
-# to run the file automatic without running code
-# pythonw file.py
+
